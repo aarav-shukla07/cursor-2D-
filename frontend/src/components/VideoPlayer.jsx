87b7667b3c1fcd9232fa493f3bd855f9cdbbc554
Aarav Shukla <aarav.shukla105@gmail.com>
2025-05-21 23:26:56 +0530

    Updated VideoPlayer.jsx file

--- frontend/src/components/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-const VideoPlayer = () => {
+const VideoPlayer = ({ videoURL }) => {
   return (
-    <div>
-      <h2>Animation Preview</h2>
-      <video width="640" height="480" controls>
-        <source src="http://localhost:5000/get_video" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+    <div style={{ marginTop: "1rem" }}>
+      <video src={videoURL} controls autoPlay muted width="480" />
     </div>
   );
 };
